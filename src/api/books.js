119ecdbd7407692ed2323c53bf0fca128d5994a0
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-06-06 18:31:20 +0530

    updateBooks  added

--- src/api/books.js
+++ src/api/books.js
@@ -25,3 +25,20 @@ export async function getBooks(){
 
   return books;
 }
+
+// TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const updateBook = async (id, newTitle, newStart, newEnd) => {
+    const response = await fetch(`${API_ENDPOINT}/book/${id}`,{
+        method : "PUT",
+        body : JSON.stringify({
+            newTitle,
+            newStart,
+            newEnd
+        }),
+        headers : {
+            "Content-Type": "application/json",
+        }
+    })
+
+    return response.status;
+};
